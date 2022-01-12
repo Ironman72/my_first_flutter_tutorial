d1002f8557e95d2d20cac2b217c9348298a68043
Ironman72 <jarvisismyheart@gmail.com>
2022-01-12 12:07:13 +0530

    learned row

--- lib/main.dart
+++ lib/main.dart
@@ -10,94 +10,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter APP',
-        home: Scaffold(
+      title: 'FLutter App',
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
           appBar: AppBar(
             centerTitle: true,
-            title: Text('List View'),
+            title: Text('Rows and Columns'),
+            backgroundColor: Colors.purple,
           ),
-          body: Material(
-            child: ListView(
-              children: [
-                Container(
-                  color: Colors.amber[400],
-                  child: ListTile(
-                    leading: Icon(Icons.panorama_fisheye_outlined),
-                    title: Text('Mr-Circle'),
-                    subtitle: Text('Waiting For Responce'),
-                    trailing: Icon(Icons.ac_unit),
-                  ),
-                ),
-                Container(
-                  color: Colors.pink[400],
-                  child: ListTile(
-                      leading: Icon(
-                        Icons.radio,
-                      ),
-                      title: Text('Virus'),
-                      subtitle: Text('Praise The Lord'),
-                      trailing: Icon(Icons.local_hospital_rounded)),
-                ),
-                Container(
-                  color: Colors.green[400],
-                  child: ListTile(
-                      leading: Icon(Icons.park),
-                      title: Text('Jarvis'),
-                      subtitle: Text('Good Morning..!'),
-                      trailing: Icon(Icons.hotel)),
-                ),
-                Container(
-                  color: Colors.blueGrey[400],
-                  child: ListTile(
-                      leading: Icon(Icons.grade),
-                      title: Text('Innocent'),
-                      subtitle: Text('Happy New Year'),
-                      trailing: Icon(Icons.view_in_ar)),
-                ),
-                Container(
-                  color: Colors.teal[400],
-                  child: ListTile(
-                      leading: Icon(Icons.park),
-                      title: Text('Mark42'),
-                      subtitle: Text('Item 4..!'),
-                      trailing: Icon(Icons.pause_circle_filled)),
-                ),
-                Container(
-                  color: Colors.purple[900],
-                  child: ListTile(
-                      leading: Icon(Icons.park),
-                      title: Text('Steve'),
-                      subtitle: Text('Item 5..!'),
-                      trailing: Icon(Icons.play_circle)),
-                ),
-                Container(
-                  color: Colors.purple[400],
-                  child: ListTile(
-                      leading: Icon(Icons.park),
-                      title: Text('George'),
-                      subtitle: Text('Item 6..!'),
-                      trailing: Icon(Icons.help)),
-                ),
-                Container(
-                  color: Colors.green[400],
-                  child: ListTile(
-                      leading: Icon(Icons.park),
-                      title: Text('Jarvis'),
-                      subtitle: Text('Good Morning..!'),
-                      trailing: Icon(Icons.hotel)),
-                ),
-                Container(
-                  color: Colors.green[400],
-                  child: ListTile(
-                      leading: Icon(Icons.park),
-                      title: Text('Jarvis'),
-                      subtitle: Text('Good Morning..!'),
-                      trailing: Icon(Icons.hotel)),
-                ),
-              ],
-            ),
+          body: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Its a Row',
+                style: TextStyle(fontSize: 15.0),
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: Text('Click Me'),
+                color: Colors.amber,
+              ),
+              Container(
+                color: Colors.cyan,
+                padding: EdgeInsets.all(30.0),
+                child: Text('This is a container'),
+              )
+            ],
           ),
-        ));
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Text('Click'),
+            backgroundColor: Colors.purple,
+          ),
+        ),
+      ),
+    );
   }
 }
